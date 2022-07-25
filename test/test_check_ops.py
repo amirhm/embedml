771e7d0ce8ebe663cd97afaa4d61a492d6d18c6b
Hossein Mamaghanian <hossein.mamaghanian@advancedsilicon.com>
2022-07-25 15:32:44 +0200

    updated the test operations

--- test/test_check_ops.py
+++ test/test_check_ops.py
@@ -27,4 +27,16 @@ def test_addition():
     x = Tensor.ones((3, 3))
     y = Tensor.eye(3)
     z = x + y
-    np.allclose(z.cpu(), znp)
+    assert np.allclose(z.cpu(), znp)
+
+
+def test_mult():
+    xnp = np.ones((3, 3))
+    ynp = np.eye(3)
+    znp = 2 * xnp + ynp * 4
+
+    x = Tensor.ones((3, 3))
+    y = Tensor.eye(3)
+    z = 2 * x + y * 4
+    assert np.allclose(z.cpu(), znp)
+
