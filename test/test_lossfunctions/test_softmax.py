c3b5b4f52ddbcb6f9caedc6300383200df3f3d92
Hossein Mamaghanian <amirhm@gmail.com>
2022-08-05 13:04:41 +0200

    softmax loss test updated (is fail: to impl div on tensor class)

--- test/test_lossfunctions/test_softmax.py
+++ test/test_lossfunctions/test_softmax.py
@@ -13,16 +13,41 @@ def test_softmax():
     r = (x - x.max(dim=1, keepdim=True).values)
     e = r.exp()
     s = e.sum(dim=1, keepdim=True)
-    sm_th = e / s
+    sm = e / s
 
-    assert np.allclose(sm_th.detach().numpy(), sm_out.detach().numpy())
+    assert np.allclose(sm.detach().numpy(), sm_out.detach().numpy())
 
     xt = Tensor(x.detach().numpy())
     rt = xt - xt.max(axis=1, keepdims=True)
     et = rt.exp()
     st = et.sum(axis=1)
-    # sm_t = e / s
+    smt = et / st
 
     assert np.allclose(r.detach().numpy(), rt.data)
     assert np.allclose(e.detach().numpy(), et.data)
     assert np.allclose(s.detach().numpy(), st.data)
+    assert np.allclose(sm.detach().numpy(), smt.data)
+
+
+def test_softmax_grad():
+    x = torch.empty((5, 10)).random_().requires_grad_(True)
+    r = (x - x.max(dim=1, keepdim=True).values)
+    e = r.exp()
+    s = e.sum(dim=1, keepdim=True)
+    sm = e / s
+
+    list(map(lambda x: x.retain_grad(), [r, e, s, sm]))
+    sm.sum().backward()
+
+    xt = Tensor(x.detach().numpy())
+    rt = xt - xt.max(axis=1, keepdims=True)
+    et = rt.exp()
+    st = et.sum(axis=1)
+    smt = et / st
+
+    smt.sum().backward()
+
+    assert np.allclose(r.grad, rt.grad.data)
+    assert np.allclose(e.grad, et.grad.data)
+    assert np.allclose(s.grad, st.grad.data)
+    assert np.allclose(sm.grad, smt.grad.data)
