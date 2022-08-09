63f6056c3ccbc7c0742df119e217aca643815db0
Hossein Mamaghanian <amirhm@gmail.com>
2022-08-09 22:53:23 +0200

    max ops fixed by defining the borad_max for backward, to be integrated with other board extend . fixes #12, #11

--- test/test_ops/test_max.py
+++ test/test_ops/test_max.py
@@ -1,11 +1,13 @@
 import torch
 import numpy as np
 from embedml.tensor import Tensor
+import pytest
 
 
-def test_max_grad():
+@pytest.mark.parametrize('axis', [(0), (1), (None)])
+def test_max(axis):
     x = torch.empty((5, 10)).random_().requires_grad_(True)
-    m = x.max(dim=1, keepdim=True).values
+    m = x.max(dim=axis, keepdim=True).values if axis is not None else x.max()
     r = (x - m)
     z = r.sum()
     m.retain_grad()
@@ -14,8 +16,48 @@ def test_max_grad():
     z.backward()
 
     xt = Tensor(x.detach().numpy())
-    mt = xt.max(axis=1, keepdims=True)
+    mt = xt.max(axis=axis, keepdims=True) if axis is not None else xt.max()
     rt = (xt - mt)
     zt = rt.sum()
     zt.backward()
     assert np.allclose(zt.data, z.detach().numpy())
+
+
+@pytest.mark.parametrize('axis', [(0), (1), (2), (None)])
+def test_max_grad0(axis):
+    x = torch.empty((5, 20, 10)).random_().requires_grad_(True)
+    m = x.max(dim=axis, keepdim=True).values if axis is not None else x.max()
+    z = m.sum() * 123
+    m.retain_grad()
+    z.retain_grad()
+    z.backward()
+
+    xt = Tensor(x.detach().numpy())
+    mt = xt.max(axis=axis, keepdims=True) if axis is not None else xt.max()
+
+    zt = mt.sum() * 123
+    zt.backward()
+    assert np.allclose(zt.data, z.detach().numpy())
+    assert np.allclose(xt.grad.data, x.grad)
+    assert np.allclose(mt.grad.data, m.grad)
+
+
+@pytest.mark.parametrize('axis', [(0), (1)])
+def test_max_grad(axis):
+    x = torch.empty((5, 10)).random_().requires_grad_(True)
+    m = x.max(dim=axis, keepdim=True).values
+    r = (x - m)
+    z = r.sum() * 10
+    m.retain_grad()
+    r.retain_grad()
+    z.retain_grad()
+    z.backward()
+
+    xt = Tensor(x.detach().numpy())
+    mt = xt.max(axis=axis, keepdims=True)
+    rt = (xt - mt)
+    zt = rt.sum() * 10
+    zt.backward()
+    assert np.allclose(zt.data, z.detach().numpy())
+    assert np.allclose(xt.grad.data, x.grad)
+    assert np.allclose(mt.grad.data, m.grad)
