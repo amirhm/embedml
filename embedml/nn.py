41e052557e8933d3e8644ad8ea3c159e47ef4c4c
Hossein Mamaghanian <amirhm@gmail.com>
2022-08-29 22:22:12 +0200

    fixed the initialization for linear modules

--- embedml/nn.py
+++ embedml/nn.py
@@ -30,8 +30,9 @@ class Linear(Module):
     def __init__(self, feature_in, feature_out):
         self.fin = feature_in
         self.fout = feature_out
-        self.weight = Tensor(np.random.randn(self.fin, self.fout), requires_grad=True)
-        self.bias = Tensor(np.random.randn(self.fout), requires_grad=True)
+        stdev = 1.0 / np.sqrt(self.fin)
+        self.weight = Tensor(np.random.uniform(-stdev, stdev, (self.fin * self.fout)).reshape(self.fin, self.fout), requires_grad=True)
+        self.bias = Tensor(np.random.uniform(-stdev, stdev, self.fout).reshape(self.fout), requires_grad=True)
 
     def forward(self, x):
         return x.matmul(self.weight) + self.bias
