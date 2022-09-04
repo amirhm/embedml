e4b7af3c44d1cc6a3e51002e02883f02ebc238b4
Hossein Mamaghanian <amirhm@gmail.com>
2022-09-04 14:10:13 +0200

    added the draw function

--- embedml/util.py
+++ embedml/util.py
@@ -1,5 +1,6 @@
 from abc import ABC
 from abc import abstractmethod
+import importlib
 import numpy as np
 
 
@@ -40,3 +41,29 @@ class DataLoader:
 
     def __iter__(self):
         return self
+
+
+def draw(root):
+    graph = importlib.import_module('graphviz')
+
+    def get_name():
+        num = 0
+        while True:
+            yield f"tensor_{num}"
+            num += 1
+
+    g = graph.Digraph(format='svg', graph_attr={'rankdir': 'TB'})
+    tensor_dict = dict()
+    r = get_name()
+    for node in reversed(list(root.get_topo_graph())):
+        for p in node.ctx.parents:
+            if tensor_dict.get(id(p), 0) == 0:
+                tensor_dict[id(p)] = next(r)
+                color = 'green' if p.requires_grad else 'black'
+                g.node(tensor_dict[id(p)], color=color)
+            g.edge(tensor_dict[id(p)], str((node.ctx)))
+        if tensor_dict.get(id(node), 0) == 0:
+            tensor_dict[id(node)] = next(r)
+            g.node(str(node.ctx), label=node.ctx.__class__.__name__, color='red')
+        g.edge(str((node.ctx)), tensor_dict[id(node)])
+    return g
