178a29e80a3cac42dabf651cc09a67433ac0a544
Hossein Mamaghanian <amirhm@gmail.com>
2022-09-05 23:40:41 +0200

    dropout test

--- test/test_ops/test_dropout.py
+++ test/test_ops/test_dropout.py
@@ -5,9 +5,14 @@ import numpy as np
 
 def test_droupout():
 
-    x = torch.arange(-10, 10, 0.01).float()
+    x = torch.arange(-10, 10, 2).float()
     lt = torch.nn.Dropout(0.5)
     yt = lt(x)
+    assert np.allclose(yt[yt != 0], x[yt != 0] * 2)
 
     xe = Tensor(np.array(x))
     ye = xe.dropout(0.5)
+    print(ye[yt.data != 0].data, xe[yt.data != 0].data * 2)
+    y, x = ye.data, xe.data * 2
+    assert np.allclose(y[y != 0], x[y != 0])
+    print(ye.data, xe.data)
