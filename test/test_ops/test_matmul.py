2850a5fc2b9b42c8c879363d190979e7ac7e5c4e
Hossein Mamaghanian <amirhm@gmail.com>
2022-09-12 23:52:54 +0200

    added test for nd-matmul

--- test/test_ops/test_matmul.py
+++ test/test_ops/test_matmul.py
@@ -1,5 +1,6 @@
 import numpy as np
 from embedml.tensor import Tensor
+import torch
 
 
 def test_simple():
@@ -17,3 +18,39 @@ def test_simple():
     assert np.allclose(x, xnp)
     assert np.allclose(y, ynp)
     assert np.allclose(z.cpu(), znp)
+
+
+def test_matmul():
+    xp = Tensor.ones((10, 3, 4, 5))
+    yp = Tensor.ones((10, 3, 5, 6))
+    zp = xp.matmul(yp)
+    assert zp.shape == (10, 3, 4, 6)
+
+
+def test_matmul_nd():
+    xt = torch.randn(3, 2, 4, 5)
+    yt = torch.randn(3, 2, 5, 6)
+    zt = xt.matmul(yt)
+
+    x = Tensor(xt)
+    y = Tensor(yt)
+    z = x.matmul(y)
+
+    assert np.allclose(z.data, zt, rtol=1e-5, atol=1e-5)
+
+
+def test_matmul_nd_grad():
+    xt = torch.randn(3, 2, 4, 5).float().requires_grad_(True)
+    yt = torch.randn(3, 2, 5, 6).float().requires_grad_(True)
+
+    zt = xt.matmul(yt).sum()
+    zt.backward()
+
+    x = Tensor(xt.detach().numpy())
+    y = Tensor(yt.detach().numpy())
+    z = x.matmul(y).sum()
+    z.backward()
+    assert np.allclose(x.grad.data, xt.grad, rtol=1e-5, atol=1e-5)
+    assert np.allclose(y.grad.data, yt.grad, rtol=1e-5, atol=1e-5)
+    assert not xt.grad.requires_grad
+    assert not yt.grad.requires_grad
